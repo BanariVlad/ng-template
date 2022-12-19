9bf535836e9d25c770ba2cf9b55c68a1bd36f68d
BanariVlad <vladislavanton@mail.ru>
2022-12-19 22:55:45 +0200

    TrackBy directive

---
+++ src/app/directives/track-by-property.directive.ts
@@ -0,0 +1,21 @@
+import { Directive, Host, Input } from '@angular/core';
+import { NgForOf } from '@angular/common';
+
+@Directive({
+  selector: '[ngForKey]',
+})
+export class TrackByPropertyDirective {
+  private property: string = '';
+
+  public constructor(@Host() private readonly ngFor: NgForOf<any>) {
+    this.ngFor.ngForTrackBy = (index: number, item: any) =>
+      this.property && typeof item === 'object' && this.property in item
+        ? item[this.property]
+        : index;
+  }
+
+  @Input('ngForKey')
+  public set propertyName(value: string) {
+    this.property = value;
+  }
+}
