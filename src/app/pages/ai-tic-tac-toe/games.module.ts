a86082e5e3c5850648e1229cb729d6bec936125c
BanariVlad <vladislavanton@mail.ru>
2023-04-02 19:35:29 +0300

    Tic Tac Toe

--- src/app/pages/ai-tic-tac-toe/games.module.ts
+++ src/app/pages/ai-tic-tac-toe/games.module.ts
@@ -1,19 +1,28 @@
+import { GameComponent } from '@/pages/games/multi-player/game.component';
 import { MaterialModule } from '@/shared/modules/material-module.module';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Route, RouterModule } from '@angular/router';
-import { GamesComponent } from './games.component';
+import { GamesComponent } from './single-player/games.component';
 
 const routes: Route[] = [
   {
-    path: '',
+    path: 'single-player',
     component: GamesComponent,
+  },
+  {
+    path: 'multi-player',
+    component: GameComponent,
+    data: {
+      index: 100,
+    },
   },
 ];
 
 @NgModule({
-  declarations: [GamesComponent],
+  // todo rename components
+  declarations: [GamesComponent, GameComponent],
   imports: [
     CommonModule,
     FormsModule,
